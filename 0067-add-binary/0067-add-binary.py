7af08f4a2b4505c82447d2c86835a2a674b3e360
VAMSIDHAR REDDY GODULA <godulavamsidharreddy@gmail.com>
2026-08-18 19:26:40 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.84%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,9 +1,4 @@
 class Solution(object):
     def addBinary(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
         x, y = int(a,2), int(b,2)
         return bin(x+y)[2:]
