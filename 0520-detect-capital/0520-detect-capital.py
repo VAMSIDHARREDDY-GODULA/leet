76e347dbdd9e8eeb7cfd97b60b329837cd7fe9a7
VAMSIDHAR REDDY GODULA <godulavamsidharreddy@gmail.com>
2026-08-22 17:38:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.25%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        if str(word).capitalize()==word or str(word).upper()==word or str(word).lower()==word:
+            return 3==3
+        return 4==3
