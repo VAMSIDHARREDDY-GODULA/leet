a4b06617edf2373dda7d159530f82713f3d90899
VAMSIDHAR REDDY GODULA <godulavamsidharreddy@gmail.com>
2026-09-05 17:45:26 +0530

    Time: 0 ms (100%), Space: 12.3 MB (98.61%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,6 +2,6 @@ class Solution(object):
     def subsets(self, nums):
         b = []
         for i in range(len(nums)+1):
-            for i in itertools.combinations(nums,i):
-                b.append(list(i))
+            for j in itertools.combinations(nums,i):
+                b.append(j)
         return b
