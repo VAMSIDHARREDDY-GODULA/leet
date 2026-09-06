b2ccd7b47c97ca70ff199b3ddc96e212ff40512b
VAMSIDHAR REDDY GODULA <godulavamsidharreddy@gmail.com>
2026-09-06 15:07:11 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0.35%) - LeetHub

--- 4020-elevator-requests-i/4020-elevator-requests-i.py
+++ 4020-elevator-requests-i/4020-elevator-requests-i.py
@@ -2,6 +2,6 @@ class Solution(object):
     def elevatorRequests(self, n, r):
         x = a = 0
         for i in r:
-            x += abs(a-i)
+            x += a-i if a>i else i-a
             a = i
         return x
